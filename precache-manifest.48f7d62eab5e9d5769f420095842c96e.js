f691c1f44a63c884ae4b449534e55b5f28f9cb87
Cristian Abrante <cristian@abrante.me>
2019-06-05 01:05:27 +0100

    Updates

---
+++ precache-manifest.48f7d62eab5e9d5769f420095842c96e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8f69bdc063f125fbff33f679ca53d4",
+    "url": "/GeneticsJsKnapsack/index.html"
+  },
+  {
+    "revision": "15a1e4caafe49f854f2f",
+    "url": "/GeneticsJsKnapsack/static/css/main.0547b71a.chunk.css"
+  },
+  {
+    "revision": "b10304984adc7717524e",
+    "url": "/GeneticsJsKnapsack/static/js/2.20d90ac6.chunk.js"
+  },
+  {
+    "revision": "15a1e4caafe49f854f2f",
+    "url": "/GeneticsJsKnapsack/static/js/main.c6d44670.chunk.js"
+  },
+  {
+    "revision": "653d3f2316ba05e991b4",
+    "url": "/GeneticsJsKnapsack/static/js/runtime~main.fe1c0819.js"
+  },
+  {
+    "revision": "18a5ff38710a927bd625153a016c46f0",
+    "url": "/GeneticsJsKnapsack/static/media/logo-letters.18a5ff38.png"
+  }
+]);
